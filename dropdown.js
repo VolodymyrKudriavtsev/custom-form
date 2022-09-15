9b4a7a9c6de5259c90c59a9785a2b1fd5d0e698b
Volodymyr Kudriavtsev <vladimirkud@ukr.net>
2022-09-15 22:13:36 +0300

    done

--- dropdown.js
+++ dropdown.js
@@ -1,16 +1,40 @@
-// Клік по кнопці: відкрити/закрити дропдаун
-document.querySelector('.dropdown__button').addEventListener('click', function () {
-    document.querySelector('.dropdown__list').classList.toggle('dropdown__list--visible');
+document.querySelectorAll('.dropdown').forEach(function (dropDownWrapper) {
+  // Змінні
+    const dropDownBtn = dropDownWrapper.querySelector('.dropdown__button');
+    const dropDownList = dropDownWrapper.querySelector('.dropdown__list');
+    const dropDownListItems = dropDownList.querySelectorAll('.dropdown__list-item');
+    const dropDownInput = dropDownWrapper.querySelector('.dropdown__input--hidden');
+
+  // Клік по кнопці: відкрити/закрити дропдаун
+    dropDownBtn.addEventListener('click', function () {
+    dropDownList.classList.toggle('dropdown__list--visible');
     this.classList.add('dropdown__button--active');
-});
-
-// Обрати елемент зі списку. Запам'ятати обране значення. Закрити дропдаун
-document.querySelectorAll('.dropdown__list-item').forEach(function (listitem) {
-    listitem.addEventListener('click', function () {
-        document.querySelector('.dropdown__button').innerText = this.innerText;
-        document.querySelector('.dropdown__button').focus();
-        document.querySelector('.dropdown__input--hidden').value = this.dataset.value;
-
     });
 
-})
+  // Обрати елемент зі списку. Запам'ятати обране значення. Закрити дропдаун
+    dropDownListItems.forEach(function (listitem) {
+    listitem.addEventListener('click', function (event) {
+        event.stopPropagation();
+        dropDownBtn.innerText = this.innerText;
+        dropDownBtn.focus();
+        dropDownInput.value = this.dataset.value;
+        dropDownList.classList.remove('dropdown__list--visible');
+    });
+    });
+
+  // Клік ззовні дропдауна закриває дропдаун
+    document.addEventListener('click', function (event) {
+    if (event.target !== dropDownBtn) {
+        dropDownBtn.classList.remove('dropdown__button--active');
+        dropDownList.classList.remove('dropdown__list--visible');
+    }
+    });
+
+  // 'Tab' або 'Esc' закривають дропдаун
+    document.addEventListener('keydown', function (event) {
+    if (event.key === 'Tab' || event.key === 'Escape') {
+        dropDownBtn.classList.remove('dropdown__button--active');
+        dropDownList.classList.remove('dropdown__list--visible');
+    }
+    });
+});
